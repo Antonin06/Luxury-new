48698c8da000c61dd4ebab9d054dcb65f88b1485
Antonin06 <avon.antonin@gmail.com>
2020-07-23 15:26:13 +0200

    ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\JobOffer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+      $jobOffers = $this->getDoctrine()
+          ->getRepository(JobOffer::class)
+          ->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'job_offers' => $jobOffers,
         ]);
     }
 }
